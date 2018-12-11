1401cbedd89f208394403c9ce4182c8f475b31df
J. M. Becker <cyberpunkspike@gmail.com>
2018-12-10 20:06:11 -0500

    Initial Update

---
+++ Cron/RefreshCache.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Cyberpunkspike\AutoRefreshCache\Cron;
+
+class RefreshCache
+{
+
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Execute the cron
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        $this->logger->addInfo("Cronjob RefreshCache is executed.");
+    }
+}
